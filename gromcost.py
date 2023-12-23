960cae01fd17a1b5f30002ce0bd51b94589f0c75
Irina <yaurio@yandex.ru>
2023-12-24 00:20:14 +0500

    Начальное окно с музыкой, и регулеровкой громкости

---
+++ gromcost.py
@@ -0,0 +1,12 @@
+import pygame as pg
+from batton import all_sprites
+
+
+class Gromcost(pg.sprite.Sprite):
+    def __init__(self, sc, x):
+        super().__init__(all_sprites)
+        self.sc = sc
+        self.image = pg.image.load('res/reg.png').convert_alpha()
+        self.image = pg.transform.scale(self.image, (x, 50))
+        self.rect = self.image.get_rect().move(1250, 800)
+
